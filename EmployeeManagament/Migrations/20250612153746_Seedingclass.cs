b6da83a4621c1d79c30877335bfa9c7e7e808749
= <=>
2025-07-13 19:53:37 +0200

    Initial commit of Employee Management solution

---
+++ EmployeeManagament/Migrations/20250612153746_Seedingclass.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EmployeeManagament.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seedingclass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "mary@pragimtech.com", "Mary" },
+                    { 2, 2, "john@pragimtech.com", "John" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
---
+++ EmployeeManagament/Models/IEmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeManagament.Models
+{
+    public interface IEmployeeRepository
+    {
+        Employee GetEmployee(int Id);
+        IEnumerable<Employee> GetAllEmployee();
+        Employee Add(Employee employee);
+        Employee Update(Employee employeeChanges);
+        Employee Delete(int Id);
+    }
+}
